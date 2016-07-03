b051398d264b1645003f3ddb8bbd09c094c8ad4d
Hagen Paul Pfeifer <hagen@jauu.net>
2016-07-03 23:18:55 +0200

    enable service
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- templates/shared/fs-copy-finish.py
+++ templates/shared/fs-copy-finish.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
-print("nothing to do")
+import os
+
+os.system("sudo systemctl enable finalizer.service")
+os.system("sudo systemctl start finalizer.service")
+os.system("sudo systemctl status finalizer.service")
 
