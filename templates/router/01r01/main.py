8a015aeceff752a4ec67e6a88db9e233c4c9f5de
Hagen Paul Pfeifer <hagen.pfeifer@rohde-schwarz.com>
2016-08-25 13:51:31 +0200

    router now uses aiohttp for REST handling
    Signed-off-by: Hagen Paul Pfeifer <hagen.pfeifer@rohde-schwarz.com>

--- templates/router/01r01/main.py
+++ templates/router/01r01/main.py
@@ -2,9 +2,9 @@ import os
 import shutil
 
 def install_packages(utils):
-    packages = "tcpdump python3-flask build-essential bison flex gdb strace screen zsh "
+    packages = "tcpdump python3 python3-aiohttp build-essential bison flex gdb strace screen zsh "
     # performacne test tools, just make sure everybody is happy
-    packages += "iperf3 netperf netcat-openbsd hping3 perf-tools-unstable"
+    packages += "iperf3 netcat-openbsd hping3 perf-tools-unstable"
     utils.install_packages(packages)
 
 def copy_global_shared_tree(utils):
